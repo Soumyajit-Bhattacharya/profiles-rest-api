e31d81d8fbe6ee0a358fa46f23478fc5bdff7207
Soumyajit Bhattacharya <soumyajit.official1995@gmail.com>
2022-05-20 14:42:48 +0530

    serializers

---
+++ profile_api/serializers.py
@@ -0,0 +1,6 @@
+import imp
+from rest_framework import serializers
+
+class HelloSerializer(serializers.Serializer):
+    """Serializes a name field for testing"""
+    name = serializers.CharField(max_length=10)
--- profile_api/views.py
+++ profile_api/views.py
@@ -1,8 +1,12 @@
+from urllib import response
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from profile_api import serializers
 
 class HelloApiView(APIView):
     """Test API View"""
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Returns list of API view features"""
@@ -13,3 +17,17 @@ class HelloApiView(APIView):
         ]
 
         return Response({'messgae': 'hello', 'an_apiview': an_apiview})
+    
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get(name)
+            message = f'hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )

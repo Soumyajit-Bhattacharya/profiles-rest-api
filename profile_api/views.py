486b7942e1129a8751e063e53a1dcd02fdfe310e
Soumyajit Bhattacharya <soumyajit.official1995@gmail.com>
2022-05-20 13:17:04 +0530

    added views

--- profile_api/views.py
+++ profile_api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloApiView(APIView):
+    """Test API View"""
+
+    def get(self, request, format=None):
+        """Returns list of API view features"""
+        an_apiview = [
+            'uses http methods',
+            'gives most control',
+            'maps manually',
+        ]
+
+        return Response({'messgae': 'hello', 'an_apiview': an_apiview})

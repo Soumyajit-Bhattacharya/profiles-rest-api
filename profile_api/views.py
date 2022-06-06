66dfb318aa1d549b680ac056cde11f7dbf50ef69
Soumyajit Bhattacharya <soumyajit.official1995@gmail.com>
2022-06-06 11:34:55 +0530

    updated views.py

--- profile_api/views.py
+++ profile_api/views.py
@@ -5,6 +5,7 @@ from rest_framework import status
 from rest_framework import viewsets
 from profile_api import models, permissions, serializers
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
 
 
@@ -100,3 +101,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
